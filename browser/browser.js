9608ac4a37ba51f3c9bd57ecb009f1872432a54f
Alasdair Morrison <amorri40@gmail.com>
2017-07-15 20:09:07 +0100

    Modified browser code to show marterialize header and remove the iframe

--- browser/browser.js
+++ browser/browser.js
@@ -2,35 +2,76 @@ var Clusterize = require('clusterize.js');
 window.$ = require('jquery');
 var _ = require('lodash');
 require('../node_modules/clusterize.js/clusterize.css');
+// require('./cluster.jsx');
+require('./browser.css');
+require('materialize-css/dist/css/materialize.min.css');
+//require('materialize-css/dist/js/materialize.min.js');
 
-console.log('about to clusterize');
-var data = ['<tr>…</tr>', '<tr>…</tr>'];
-_.times(10000, function(i) {data.push(`<tr><p>${i}</p></tr>`);});
-var clusterize = new Clusterize({
-  rows: data,
-  scrollId: 'scrollArea',
-  contentId: 'contentArea',
-  rows_in_block:50,
-  callbacks: {
-    clusterWillChange: function(a,b,c) {
-        console.log('Cluster about to change',a,b,c);
-    }
-  }
+import Rx from 'rx';
+window.subject = new Rx.Subject();
+
+var subscription = subject.subscribe(function (data) {
+	console.log('data: ' + data);
 });
 
-var iframes = $('iframe');
 
-    $('button').click(function() {
-        iframes.attr('src', function() {
-            return $(this).data('src');
-        });
-    });
 
-    iframes.attr('data-src', function() {
-        var src = $(this).attr('src');
-        $(this).removeAttr('src');
-        return src;
+console.log('about to clusterize');
+
+const colour_map = {
+    data: '#FFFFCC' ,
+    code: '#CCDDFF',
+    unknown: '#FFAACC',
+    bgmap: '#CCFFCC',
+    graphics: '#CCAACC'
+}
+
+function parseAddress(addressKey) {
+    return +(addressKey.split('-')[0])
+}
+
+function setup_clusterize(game_data) {
+    var data = [];
+    _.each(Object.keys(game_data), function(a,b) {
+        var colour = colour_map[game_data[a].type];
+        // console.error(a,game_data[a]);
+        var width=( (parseAddress(a) %  0x4000) / 0x4000) *100;
+        var labelsHtml = '';
+        if (game_data[a]['labels']) {
+            console.log(game_data[a]['labels']);
+            game_data[a]['labels'].forEach(label_name => {labelsHtml+=label_name+' ';});
+        }
+        data.push(`<div  onclick="window.subject.onNext('${a}')" class='addrListElement progress-parent'>
+        <span class="progress-bar" style="width: ${width}%;background:${colour}"></span>
+        <a>${a} (<strong style="background:${colour}">${game_data[a].type}</strong>) <small>${labelsHtml}</small></a>
+        </div>`);
+    } );
+    var clusterize = new Clusterize({
+    rows: data,
+    scrollId: 'scrollArea',
+    contentId: 'contentArea',
+    rows_in_block:100,
+    callbacks: {
+        clusterWillChange: function(a,b,c) {
+            console.log('Cluster about to change',a,b,c);
+        }
+    }
     });
+}
+
+// var iframes = $('iframe');
+
+//     $('button').click(function() {
+//         iframes.attr('src', function() {
+//             return $(this).data('src');
+//         });
+//     });
+
+//     iframes.attr('data-src', function() {
+//         var src = $(this).attr('src');
+//         $(this).removeAttr('src');
+//         return src;
+//     });
 
 window.onload = function() {
         var canvas=document.getElementById("myCanvas");
@@ -55,3 +96,10 @@ window.onload = function() {
         // // we’re done with the rotating so restore the unrotated context
         context.restore();
     };
+
+    $.getJSON( "./game.json", function( game_data ) {
+        setup_clusterize(game_data);
+    });
+    // require('./events.js');
+    // require('./redux.jsx');
+    require('./romData.jsx');
